5f8bb06acc537aacf6c45bf6a3f5735e3cd85394
Owen <owen.dessein@student.vives.be>
2021-03-04 11:30:25 +0100

    Begin aanmaken class gui

---
+++ Planten2021.Domain/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planten2021.Data
+{
+    class Constants
+    {
+        public static string connectionstring = "Server=LAPTOP-0UQL3LGS\\VIVES; Database=Planten2021; Trusted_Connection=True;";
+
+    }
+}
---
+++ PlantenApplicatie/GUI/BasisGui.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantenApplicatie.GUI
+{
+    public class BasisGui
+    {
+        public void Frame_Navigated()
+        {
+            CvsZoeken.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnbackgroundColor()
+        {
+            btnZoeken.Background = Brushes.Transparent;
+        }
+    }
+}
