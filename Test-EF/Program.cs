47d43fafcde8e8098be677899a33524a740a7854
Kenny <kenny.demits@student.vives.be>
2021-03-05 16:17:37 +0100

    functie simplify() created.

--- Test-EF/Program.cs
+++ Test-EF/Program.cs
@@ -9,11 +9,19 @@ namespace Test_EF
         private static Planten2021Context context = new Planten2021Context();
         static void Main(string[] args)
         {
-            var test = context.TfgsvFamilie.FirstOrDefault(f => f.Familienaam == "ALLIACEAE");
-            string nameTest = test.Familienaam;
-            
-            Console.WriteLine("De famillienaam = " + nameTest);
-            Console.WriteLine("test");
+            string stringAwkward = "                            AA\" BB CC \"DD ee' Ff l      ";
+            Console.WriteLine(stringAwkward);
+            string stringAwkward2 = simplify(stringAwkward);
+            Console.WriteLine(stringAwkward2);
+            Console.WriteLine(simplify(stringAwkward2));
+        }
+
+        //Not even an Asian uderstands it but it works
+        public static string simplify(string stringToSimplify)
+        {
+            string answer = stringToSimplify.ToLower().Trim().Replace("\'", " ").Replace("\"", " ");
+            answer = String.Concat(answer.Where(c => !Char.IsWhiteSpace(c)));
+            return answer;
         }
     }
 }
