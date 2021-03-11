cd5abec615015af365dc6357ba028294b01c431b
Christophe Hemeryck <r0345896@student.vives.be>
2021-03-11 16:35:19 +0100

    Plantencontext connectionstring aangepast

--- Planten2021.Data/Planten2021Context.cs
+++ Planten2021.Data/Planten2021Context.cs
@@ -58,7 +58,7 @@ namespace Planten2021.Domain.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=SJMTCMFS\\VIVES; Database=Planten2021; Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(Constants.connectionstring);
             }
         }
 
