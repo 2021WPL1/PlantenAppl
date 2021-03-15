3a626342f975f87f0902a693badbb118a60d37ef
Christophe Hemeryck <r0345896@student.vives.be>
2021-03-15 11:03:13 +0100

    niet belangerijk

---
+++ Planten2021.Data/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planten2021.Domain.Models
+{
+    public static class Constants
+    {
+        public static string connectionstring = "Server=LAPTOP-STOFFEL\\VIVES;Database=Planten2021;Trusted_Connection=True;";
+    }
+}
