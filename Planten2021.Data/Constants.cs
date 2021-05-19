1eed9dc11ee8da461a0ea2ede5bf45a3a91e32fd
Kenny <kenny.demits@student.vives.be>
2021-05-19 13:48:02 +0200

    started the basic logic for reading out the Plant object. --work in progress

--- Planten2021.Data/Constants.cs
+++ Planten2021.Data/Constants.cs
@@ -6,6 +6,6 @@ namespace Planten2021.Data
 {
     class Constants
     {
-        public static string connectionstring = "Server=LAPTOP-0UQL3LGS\\VIVES; Database=Planten2021; Trusted_Connection=True;";
+        public static string connectionstring = "Server=MSI\\VIVES; Database=Planten2021; Trusted_Connection=True;";
     }
 }
--- Test-EF/Program.cs
+++ Test-EF/Program.cs
@@ -1,25 +1,55 @@
 ﻿using Planten2021.Domain.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Test_EF
 {
     class Program
     {
+
+        private static _Planten2021Context context = new _Planten2021Context();
+
         public string Simplify(string stringToSimplify)
         {
             string answer = stringToSimplify.Replace("\'", " ").Replace("\"", " ");
             answer = String.Concat(answer.Where(c => !Char.IsWhiteSpace(c)));
             return answer;
         }
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(Simplify("vebkob  regeg"));
+            _readObjectProperties<Plant>(30);
 
         }
 
-            
-        
+        private static void _readObjectProperties<T>(int Plantid)
+        {
+
+            Type t = typeof(T);
+            var objectTypeProperties = t.GetProperties();
+
+            var objectToRead = context.Plant.SingleOrDefault(p => p.PlantId == Plantid);
+
+            foreach (var p in objectTypeProperties)
+            {
+                if (_checkIfProperyIsObject(p))
+                {
+                    Console.WriteLine("this is a type itself and should be read out separately.");
+                }
+                else
+                {
+                    var propertyText = p.GetValue(objectToRead);
+                    Console.WriteLine($"{p.Name}: {propertyText} " + "\r\n");
+                }
+
+            }
+        }
+
+        private static bool _checkIfProperyIsObject(PropertyInfo info)
+        {
+            return info.PropertyType.GetInterface(nameof(ICollection)) != null;
+        }
     }
 }
