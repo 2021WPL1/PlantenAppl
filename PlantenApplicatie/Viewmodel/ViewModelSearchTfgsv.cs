763926ea37751a9765f9dab8af75e1bac4ca9342
Owen <owen.dessein@student.vives.be>
2021-05-19 11:30:34 +0200

    basis

--- PlantenApplicatie/Viewmodel/ViewModelSearchTfgsv.cs
+++ PlantenApplicatie/Viewmodel/ViewModelSearchTfgsv.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Planten2021.Data;
 
 namespace PlantenApplicatie.ViewModel
 {
-    class ViewModelSearchTfgsv:ViewModelBase
+    class ViewModelSearchTfgsv
     {
-        
+        private DAO _dao;
+        public ViewModelSearchTfgsv(DAO dao)
+        {
+            _dao = dao;
+        }
+
+        //public ICommand addProductToInventoryCommand { get; set; }
+        //public ICommand removeProductFromInventoryCommand { get; set; }
+        //public ICommand addProductCommand { get; set; }
+        //public ICommand removeProductCommand { get; set; }
+
+        //public ObservableCollection<Inventory> Inventories { get; set; }
+        //public ObservableCollection<Product> Products { get; set; }
+
+        //private string productNameInput;
+        //private string productDescriptionInput;
+
+        //private GameStoreDataService _dataservice;
+
+        //private Inventory _selectedInventory;
+        //private Product _selectedProduct;
+
+        //public MainViewModel(GameStoreDataService zooAnimalDataService)
+        //{
+        //    addProductToInventoryCommand = new DelegateCommand(addToInventory);
+        //    removeProductFromInventoryCommand = new DelegateCommand(removeFromInventory);
+        //    addProductCommand = new DelegateCommand(addProduct);
+        //    removeProductCommand = new DelegateCommand(deleteProduct);
+
+        //    Inventories = new ObservableCollection<Inventory>();
+        //    Products = new ObservableCollection<Product>();
+
+        //    this._dataservice = zooAnimalDataService;
+        //}
     }
 }
