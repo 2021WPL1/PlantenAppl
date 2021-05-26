5b21895b799ff94bb44cd4135951384392fe2c5a
Owen <owen.dessein@student.vives.be>
2021-05-26 11:07:59 +0200

    test4

--- Planten2021.Data/Constants.cs
+++ Planten2021.Data/Constants.cs
@@ -1,7 +1,12 @@
-﻿namespace Planten2021.Data
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planten2021.Data
 {
-    public class Constants
+    class Constants
     {
-        public static string connectionstring = "Server=SJMTCMFS\\VIVES; Database=Planten2021; Trusted_Connection=True;";
+        //persoonlijke server, staat normaalgezien op ingore 
+        public static string connectionstring = "Server=LAPTOP-0UQL3LGS\\VIVES; Database=Planten2021; Trusted_Connection=True;";
     }
 }
