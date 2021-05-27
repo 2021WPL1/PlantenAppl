4b40d0dcff2ce3f7552332662896f6745de67438
Kenny <kenny.demits@student.vives.be>
2021-05-27 10:04:49 +0200

    Fixed the GitIgnore (by instructions of Owen)

--- Planten2021.Data/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Planten2021.Data
-{
-    public class Constants
-    {
-        public static string connectionstring = "Server=MSI\\VIVES; Database=Planten2021; Trusted_Connection=True;";
-    }
-}
