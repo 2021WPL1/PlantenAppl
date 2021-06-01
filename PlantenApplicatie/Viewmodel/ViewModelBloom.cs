15f0a48074b54381ddc760708642612a7e529f54
Christophe Hemeryck <r0345896@student.vives.be>
2021-06-01 12:24:21 +0200

    Add selectedbloeihoogte

--- PlantenApplicatie/Viewmodel/ViewModelBloom.cs
+++ PlantenApplicatie/Viewmodel/ViewModelBloom.cs
@@ -1,13 +1,36 @@
 ﻿using PlantenApplicatie.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Planten2021.Data;
 
 
 namespace PlantenApplicatie.Viewmodel
 {
     public class ViewModelBloom : ViewModelBase
     {
-        public ViewModelBloom() { }
+        private DAO _dao;
+
+        public ViewModelBloom()
+        {
+            this._dao = DAO.Instance();
+
+        }
+
+        private string _selectedBloeiHoogte;
+
+        public string SelectedBloeiHoogte
+        {
+            get { return _selectedBloeiHoogte; }
+            set
+            {
+                _selectedBloeiHoogte = value;
+                OnPropertyChanged();
+
+            }
+        }
+
     }
 }
+
